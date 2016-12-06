7d52fe7e6bbf802203c668595fd581b00d14f4dd
Dimitris Fasoulas <dimitris@globalmouth.com>
2016-12-06 17:17:14 +0100

    Revert "Changed default params"
    This reverts commit 2fecee156d4fb73af0bd3e474ab1ffa5e0bc1b34.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -7,8 +7,8 @@ import store from './Store/store';
 coreLibrary.init({
    widgetTrackingName: 'gm-combo-widget',
    sport: 'FOOTBALL',
-   defaultListLimit: 2, // A default setting for the size of the list, used when resetting
-   selectionLimit: 5, // The maximum allowed selections, the bet slip supports up to 12 outcomes
+   defaultListLimit: 3, // A default setting for the size of the list, used when resetting
+   selectionLimit: 12, // The maximum allowed selections, the bet slip supports up to 12 outcomes
    replaceOutcomes: true, // When selecting a different outcome in a betoffer that has already been added to the betslip, should we replace it?
    oddsRange: [1, 8] // An array containing 2 numeric values. Widget will filter out events that don't have betoutcomes inside this range
 })
