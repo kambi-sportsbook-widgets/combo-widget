b1e9f70cf63313615bc2fe0b3de0735d3fd2687e
Karol Maciaszek <karol@globalmouth.com>
2017-03-29 16:34:14 +0200

    Increased core-library version to use 1.0.0.24 widget api, small fixes

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -12,12 +12,16 @@ coreLibrary.init({
    replaceOutcomes: true, // When selecting a different outcome in a betoffer that has already been added to the betslip, should we replace it?
    oddsRange: [1, 8] // An array containing 2 numeric values. Widget will filter out events that don't have betoutcomes inside this range
 })
-.then(() => store.getEvents(coreLibrary.args.sport, coreLibrary.args.defaultListLimit, coreLibrary.args.oddsRange.sort()))
+.then(() => store.getEvents(
+   coreLibrary.args.sport,
+   coreLibrary.args.defaultListLimit,
+   coreLibrary.args.oddsRange.concat().sort() // source array is sealed, using concat() to make a copy
+))
 .then((events) => {
    if (events.length <= 1) {
       throw new Error('Not enough events')
    }
-   coreLibrary.setWidgetTrackingName(coreLibrary.args.widgetTrackingName);
+   coreLibrary.widgetTrackingName = coreLibrary.args.widgetTrackingName;
 
    ReactDOM.render(
       <ComboWidget
